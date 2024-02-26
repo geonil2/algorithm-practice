0a436bd8d888695c2d0de912abe4bfe81c13ec54
geonil2 <83754159+geonil2@users.noreply.github.com>
2024-02-26 10:43:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.27 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,9 @@
 function solution(participant, completion) {
-    const map = new Map();
+    participant.sort();
+    completion.sort();
     
-    participant.forEach((name) => {
-        if (map.has(name)) {
-            map.set(name, map.get(name) + 1);
-        } else {
-            map.set(name, 1);
-        }
-    })
-    
-    completion.forEach((name) => {
-        if (map.has(name)) {
-            map.set(name, map.get(name) - 1);
-        }
-    })
-    
-    for (const [name, count] of map) {
-        if (count === 1) {
-            return name;
-        }
-    }   
+    for (let i = 0; i < participant.length; i++) {
+        if (participant[i] !== completion[i])
+     	return participant[i];        
+    }
 }
