716a039072ca751187d9fc797830c40530f04577
geonil2 <83754159+geonil2@users.noreply.github.com>
2024-02-26 10:48:53 +0900

    [level 0] Title: 문자열 돌리기, Time: 68.33 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    const strArray = str.split('');
+    
+    strArray.map(text => console.log(text));
+});
