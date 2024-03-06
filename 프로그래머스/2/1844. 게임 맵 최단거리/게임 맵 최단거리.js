3da5a9d90aaa6d76fbe5e61c1f6f1e3a05c870d9
geonil2 <83754159+geonil2@users.noreply.github.com>
2024-03-06 16:49:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 26.54 ms, Memory: 40.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,33 @@
+function solution(maps) {
+    const n = maps.length; // 세로 길이
+    const m = maps[0].length; // 가로 길이
+    const direction = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+    
+    function bfs(graph, startX, startY) {
+      const queue = [];
+      queue.push([startX, startY]);
+
+      while (queue.length !== 0) {
+        const [x, y] = queue.shift();
+
+        for (let i = 0; i < 4; i++) {
+          const nx = x + direction[i][0];
+          const ny = y + direction[i][1];
+
+          if (nx < 0 || nx >= n || ny < 0 || ny >= m) // map 밖으로 나가는 경우 제외
+            continue;
+
+          if (graph[nx][ny] === 1) { // 처음 방문할 경우에만 확인
+            queue.push([nx, ny]);
+            graph[nx][ny] = graph[x][y] + 1;
+          }
+        }
+      }
+    }
+    
+    bfs(maps, 0, 0)
+    console.log(maps)
+    const count = maps[n - 1][m - 1];
+    
+    return count === 1 ? -1 : count;
+}
