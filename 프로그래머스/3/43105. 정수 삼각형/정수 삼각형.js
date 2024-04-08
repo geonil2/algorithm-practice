618deb8eccfdf0520e197ec5cffe77f7e3d436e4
geonil2 <83754159+geonil2@users.noreply.github.com>
2024-04-08 19:57:57 +0900

    [level 3] Title: 정수 삼각형, Time: 6.91 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,9 +1,9 @@
 function solution(triangle) {
     const n = triangle.length;
 
-    for(let i = 1 ; i < n; i++) {
-        for (let j = 0; j <triangle[i].length; j++) {
-            triangle[i][j] += Math.max(triangle[i - 1][j - 1] || 0, triangle[i - 1][j] || 0);
+    for (let i = 1 ; i < n; i++) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            triangle[i][j] += Math.max(triangle[i - 1][j - 1] || 0, triangle[i - 1][j] || 0); // 왼쪽 위, 오른쪽 위 중 큰 값 더하기
         };
     };
 
